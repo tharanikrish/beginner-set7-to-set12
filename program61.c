3995bac12e57881cd41919c6018a568e25dc2598
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-03-12 16:50:39 +0530

    Create program61.c

---
+++ program61.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int k,i;
+char s[10];
+printf("\nenter the number:");
+scanf("%d",&k);
+printf("\nenter the string:\n");
+scanf("%s",a);
+for(i=0;i<k;i++)
+{
+printf("%s",a[i]);
+}
+return 0;
+}
